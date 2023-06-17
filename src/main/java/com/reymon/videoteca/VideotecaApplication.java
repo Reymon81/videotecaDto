87c65e90ca769852bb667f98b5553f411728398f
Reymon81 <ramon.torres.81@gmail.com>
2023-06-17 13:59:03 +0200

    proyecto vacio

---
+++ src/main/java/com/reymon/videoteca/VideotecaApplication.java
@@ -0,0 +1,13 @@
+package com.reymon.videoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reymon/videoteca/VideotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reymon.videoteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
